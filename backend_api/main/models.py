98aacc44ac921070a8154fd1ddc59cdd282229e2
Re1gns <lreigns12@gmail.com>
2024-01-11 22:37:02 +0100

    Vendor Model

---
+++ backend_api/main/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+#Vendor Model
+class Vendor(models.Model):
+    vendor = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.TextField(max_length=200)
+
+#
