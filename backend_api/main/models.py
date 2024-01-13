8e1718ed13fe3d22c8a080d9c7fd076d12e55d64
Re1gns <lreigns12@gmail.com>
2024-01-13 22:50:46 +0100

    Product category model

--- backend_api/main/models.py
+++ backend_api/main/models.py
@@ -6,6 +6,12 @@ from django.contrib.auth.models import User
 #Vendor Model
 class Vendor(models.Model):
     vendor = models.ForeignKey(User, on_delete=models.CASCADE)
-    address = models.TextField(max_length=200)
+    address = models.TextField(null=True)
 
-#
+#Products Category
+class ProductCategory(models.Model):
+    title = models.CharField(max_length=200)
+    detail = models.TextField(null=True)
+
+    def __str__(self):
+        return self.title
